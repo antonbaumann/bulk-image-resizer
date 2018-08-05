c5f291e5470385c6451dd3cfe88bb13b76b8f7d8
Anton Baumann <anton@antonbaumann.com>
2018-08-05 13:53:15 +0200

    progress output

--- resize.py
+++ resize.py
@@ -2,12 +2,50 @@ import argparse
 import os
 import pathlib
 from PIL import Image
+import time
 
 # constants
 POSSIBLE_RESULT_FORMATS = ['jpeg', 'png', 'gif']
 
 
 # functions
+def init_parser():
+	p = argparse.ArgumentParser(description='resize images')
+
+	p.add_argument('root_path', metavar='PATH_ROOT', type=str, help='path to root directory of images')
+	p.add_argument('result_path', metavar='PATH_RESULT', type=str, help='path to resized images')
+	p.add_argument('--height', metavar='H', type=int, help='height of resized image')
+	p.add_argument('--width', metavar='W', type=int, help='width of resized image')
+	p.add_argument('--format', metavar='F', type=str, default='jpeg',
+						help='format of resized image {}'.format(POSSIBLE_RESULT_FORMATS))
+	return p
+
+
+def validate_arguments(p, a):
+	if not os.path.isdir(a.root_path):
+		p.error('Input directory [{}] does not exist'.format(a.root_path))
+
+	if not os.path.isdir(a.result_path):
+		p.error('Output directory [{}] does not exist'.format(a.result_path))
+
+	if a.root_path in a.result_path and a.root_path != a.result_path:
+		p.error('Output directory must not be in input directory')
+
+	if a.root_path == a.result_path:
+		print('[!] WARNING: With this configuration your images could be overwritten!')
+		input_str = '-----'
+		while input_str.upper() not in ['Y', 'N', '']:
+			input_str = input('[?] Do you want to proceed? [y|N]: ')
+			if input_str in ['N', '']:
+				exit(0)
+
+	if a.format not in POSSIBLE_RESULT_FORMATS:
+		p.error('result image format not supported')
+
+	if a.height is not None and a.width is not None:
+		p.error('you can either set height or width')
+
+
 def get_new_size(size, args):
 	if args.height is not None:
 		ratio = args.height / size[1]
@@ -35,59 +73,72 @@ def parse_name(file_name, suffix):
 	return file_name.split('.')[0] + '.' + suffix
 
 
-# parser
-parser = argparse.ArgumentParser(description='resize images')
+def progress(t_start, nr_images, nr_done):
+	time_per_image = (time.time() - t_start) / nr_done if nr_done != 0 else 1
+	time_left_in_seconds = int(time_per_image * (nr_images - nr_done))
 
-parser.add_argument('root_path', metavar='PATH_ROOT', type=str, help='path to root directory of images')
-parser.add_argument('result_path', metavar='PATH_RESULT', type=str, help='path to resized images')
-parser.add_argument('--height', metavar='H', type=int, help='height of resized image')
-parser.add_argument('--width', metavar='W', type=int, help='width of resized image')
-parser.add_argument('--format', metavar='F', type=str, default='jpeg',
-					help='format of resized image {}'.format(POSSIBLE_RESULT_FORMATS))
+	hours = (time_left_in_seconds // 3600)
+	minutes = str((time_left_in_seconds // 60) % 60).zfill(2)
+	seconds = str(time_left_in_seconds % 60).zfill(2)
 
-args = parser.parse_args()
+	time_left_str = ''
+	if hours > 0:
+		time_left_str += '{}h {}min {}s'.format(hours, minutes, seconds)
+	else:
+		time_left_str += '{}min {}s'.format(minutes, seconds)
 
-args.root_path = os.path.abspath(args.root_path)
-args.result_path = os.path.abspath(args.result_path)
-
-# check if arguments are valid
-if not os.path.isdir(args.root_path):
-	parser.error('Input directory [{}] does not exist'.format(args.root_path))
-
-if not os.path.isdir(args.result_path):
-	parser.error('Output directory [{}] does not exist'.format(args.result_path))
-
-if args.root_path in args.result_path and args.root_path != args.result_path:
-	parser.error('Output directory must not be in input directory')
-
-if args.root_path == args.result_path:
-	print('[!] WARNING: With this configuration your images could be overwritten!')
-	input_str = '-----'
-	while input_str.upper() not in ['Y', 'N', '']:
-		input_str = input('[?] Do you want to proceed? [y|N]: ')
-		if input_str in ['N', '']:
-			exit(0)
-
-if args.format not in POSSIBLE_RESULT_FORMATS:
-	parser.error('result image format not supported')
-
-if args.height is not None and args.width is not None:
-	parser.error('you can either set height or width')
+	return '{} of {} | {}'.format(
+		str(nr_done).zfill(len(str(nr_images))),
+		nr_images,
+		time_left_str
+	)
 
 
-# iterate through every file in the root directory and all child-directories
-for root, directory, files in os.walk(args.root_path):
-	for file in files:
-		try:
+def main():
+	parser = init_parser()
+	args = parser.parse_args()
+
+	args.root_path = os.path.abspath(args.root_path)
+	args.result_path = os.path.abspath(args.result_path)
+
+	validate_arguments(parser, args)
+
+	# count files
+	nr_files_to_resize = 0
+	print('[i] counting files ...')
+	for _, _, files in os.walk(args.root_path):
+		nr_files_to_resize += len(files)
+	print('[i] {} files to resize'.format(nr_files_to_resize))
+
+	# iterate through every file in the root directory and all child-directories
+	time_start = time.time()
+	nr_files_resized = 0
+	for root, directory, files in os.walk(args.root_path):
+		for file in files:
 			structure = get_relative_path_from_root(root, args.result_path)
 			path_in = os.path.join(root, file)
 			root_out = os.path.abspath(os.path.join(args.result_path + '/' + structure))
-			im = Image.open(path_in)
-			icc_profile = im.info.get('icc_profile')
-			size = get_new_size(im.size, args)
-			im = im.resize(size, Image.ANTIALIAS)
-			pathlib.Path(root_out).mkdir(parents=True, exist_ok=True)
-			im.save(os.path.join(root_out, parse_name(file, args.format)), args.format, icc_profile=icc_profile)
-			print('resized {}'.format(path_in))
-		except IOError:
-			pass
+			try:
+				im = Image.open(path_in)
+				icc_profile = im.info.get('icc_profile')
+				size = get_new_size(im.size, args)
+				im = im.resize(size, Image.ANTIALIAS)
+				pathlib.Path(root_out).mkdir(parents=True, exist_ok=True)
+				im.save(os.path.join(root_out, parse_name(file, args.format)), args.format, icc_profile=icc_profile)
+				print('[✓] [{}] resized  {}'.format(
+					progress(time_start, nr_files_to_resize, nr_files_resized), path_in)
+				)
+			except IOError:
+				print('[i] [{}] skipping {}: file not supported'.format(
+					progress(time_start, nr_files_to_resize, nr_files_resized), path_in)
+				)
+			except ValueError:
+				print('[i] [{}] skipping {}: file not supported'.format(
+					progress(time_start, nr_files_to_resize, nr_files_resized), path_in)
+				)
+			finally:
+				nr_files_resized += 1
+
+
+if __name__ == '__main__':
+	main()
